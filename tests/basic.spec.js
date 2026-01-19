88ac9ea44f998ae087cf9355115ef57e34ef0024
Prianti Banik <59369461+prianti29@users.noreply.github.com>
2026-01-19 10:32:33 +0600

    feat: Add Playwright tests for browser context and login scenarios.

--- tests/basic.spec.js
+++ tests/basic.spec.js
@@ -6,6 +6,23 @@ test("Browser Context Playwright Test", async ({ browser }) => {
      await page.goto("https://google.com");
 });
 
+test("Login with InCorrect Credentials", async ({ page }) => {
+     await page.goto("/login");
+     //get title
+     console.log(await page.title());
+     await expect(page).toHaveTitle("Automation Exercise - Signup / Login");
+     //css , xpath
+     const email = page.locator("//input[@data-qa='login-email']");
+     const password = page.locator('input[name="password"]');
+
+     await email.fill("test1@sharklasers.com");
+     await password.fill("12345678910");
+
+     await page.getByRole('button', { name: 'Login' }).click();
+
+     await expect(page.getByText('Your email or password is incorrect!', { exact: true })).toBeVisible();
+});
+
 test("Login with Correct Credentials", async ({ page }) => {
      await page.goto("/login");
      //get title
@@ -24,19 +41,3 @@ test("Login with Correct Credentials", async ({ page }) => {
 });
 
 
-test("Login with InCorrect Credentials", async ({ page }) => {
-     await page.goto("/login");
-     //get title
-     console.log(await page.title());
-     await expect(page).toHaveTitle("Automation Exercise - Signup / Login");
-     //css , xpath
-     const email = page.locator("//input[@data-qa='login-email']");
-     const password = page.locator('input[name="password"]');
-
-     await email.fill("test1@sharklasers.com");
-     await password.fill("12345678910");
-
-     await page.getByRole('button', { name: 'Login' }).click();
-
-     await expect(page.getByText('Your email or password is incorrect!', { exact: true })).toBeVisible();
-});
